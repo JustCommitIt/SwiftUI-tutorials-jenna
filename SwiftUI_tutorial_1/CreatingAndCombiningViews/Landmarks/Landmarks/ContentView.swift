8683ec35c671b5664c9348e17a445559857b09d2
ueunli <cdenen20@gmail.com>
2023-04-21 15:46:41 +0900

    [Chapter1-1] s1. 실습 환경 준비 #1
    - Canvas
    - 새로운 SwiftUI 프로젝트 Landmark 생성

---
+++ SwiftUI_tutorial_1/CreatingAndCombiningViews/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by J.E on 2023/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_tutorial_1/CreatingAndCombiningViews/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by J.E on 2023/04/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
