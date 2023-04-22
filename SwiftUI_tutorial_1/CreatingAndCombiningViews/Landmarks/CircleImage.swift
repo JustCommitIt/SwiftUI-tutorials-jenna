f3692db020875c6c25c7eab13e242ab0a649300c
ueunli <cdenen20@gmail.com>
2023-04-22 09:54:50 +0900

    [Chapter1-1] s4. Create a Custom Image View #1
    - Image) image view
    - Circle
    - stroke, shadow

---
+++ SwiftUI_tutorial_1/CreatingAndCombiningViews/Landmarks/CircleImage.swift
@@ -0,0 +1,20 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by 유넬리 on 2023/04/22.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
--- SwiftUI_tutorial_1/CreatingAndCombiningViews/Landmarks/Landmarks/ContentView.swift
+++ SwiftUI_tutorial_1/CreatingAndCombiningViews/Landmarks/Landmarks/ContentView.swift
@@ -9,19 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-                .foregroundColor(.green)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
             }
-        }
-        .padding()
+            .shadow(radius: 7)
     }
 }
 
